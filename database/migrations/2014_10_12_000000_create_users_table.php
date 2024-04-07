c697abf30c033736ef4077b344a7aecf0c4a1638
Daniel Castillo <danielcastillo460@gmail.com>
2024-04-07 18:40:43 -0500

    Database Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,23 @@ return new class extends Migration
             $table->softDeletes();
         });
 
+        Schema::create('states', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('id_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_types_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -30,22 +45,8 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
+            $table->foreign('id_types_id')->references('id')->on('id_types');
             $table->foreign('roles_id')->references('id')->on('roles');
-        });
-
-        Schema::create('states', function (Blueprint $table) {
-            $table->id();
-            $table->string('description');
-            $table->timestamps();
-            $table->softDeletes();
-        });
-
-        Schema::create('business', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->timestamps();
-            $table->softDeletes();
         });
 
         Schema::create('appointments', function (Blueprint $table) {
@@ -54,10 +55,10 @@ return new class extends Migration
             $table->string('hour');
             $table->unsignedBigInteger('business_id');
             $table->unsignedBigInteger('users_id');
-            $table->unsignedBigInteger('state_id');
+            $table->unsignedBigInteger('states_id');
             $table->timestamps();
             $table->softDeletes();
-            $table->foreign('business_id')->references('id')->on('business');
+            $table->foreign('business_id')->references('id')->on('users');
             $table->foreign('users_id')->references('id')->on('users');
             $table->foreign('states_id')->references('id')->on('states');
         });
@@ -70,6 +71,10 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('states');
+        Schema::dropIfExists('id_types');
         Schema::dropIfExists('users');
+        Schema::dropIfExists('appointments');
     }
 };
